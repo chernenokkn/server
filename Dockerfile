ec1e3bc657e6f01abb3466ca0f599b7f59a11eb4
admin <admin@>
2023-09-13 15:28:00 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:11
 ADD build/libs/server-0.0.1.jar app.jar
-EXPOSE 8090
+EXPOSE 8080
 ENV JAVA_OPTS=""
 ENTRYPOINT exec java $JAVA_OPTS -Dspring.profiles.active=dev -jar /app.jar
--- src/main/java/com/example/server/service/impl/UserImpl.java
+++ src/main/java/com/example/server/service/impl/UserImpl.java
@@ -4,16 +4,27 @@ import com.example.server.dto.UserDto;
 import com.example.server.service.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserImpl implements User {
     @Override
     public List<UserDto> getUsers() {
+
+
+        List<UserDto> users = new ArrayList<>();
         UserDto user = new UserDto();
         user.setId("111");
         user.setLogin("admin");
-        return List.of(user);
+        users.add(user);
+
+        UserDto user1 = new UserDto();
+        user1.setId("222");
+        user1.setLogin("user");
+        users.add(user1);
+
+        return users;
     }
 
     @Override
